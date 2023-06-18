9b411e241995c76ecfbca76713b44521dc26ee33
Fabricio <banksapk@gmail.com>
2023-06-18 00:54:40 -0300

    first commit

---
+++ src/pages/Login/Login.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { DivLogin } from "./styledLogin";
+
+const Login = () => {
+  return <DivLogin>teste</DivLogin>;
+};
+
+export default Login;
---
+++ src/pages/Login/styledLogin.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const DivLogin = styled.div`
+  background-color: #d9d9d9;
+  width: 100%;
+  height: 100vh;
+`;
